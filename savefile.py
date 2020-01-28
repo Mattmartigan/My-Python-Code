6f79a464055d91aed5fd5558506fd3054b549fdc
Mattmartigan <49180495+Mattmartigan@users.noreply.github.com>
2020-01-27 20:36:34 -0700

    Delete savefile.py

--- savefile.py
+++
@@ -1,5 +0,0 @@
-ans=input("What is your next favorite color?")
-with open("D:\\Python\\Python37-32\\st.txt","w") as st:
-    st.write(ans)
-with open("D:\\Python\\Python37-32\\st.txt","r") as st:
-    print(st.read())
